aaeef1861345d9f972fe0780eb768b882b24eb4e
MarcoVilera <marcovilarev123@gmail.com>
2023-10-23 23:50:58 -0400

    Fix: fixed bill update error and minor bugs

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,192 +1,176 @@
-// Obtener todos los elementos con la clase "item"
+// Obtener los elementos del DOM (contenedores clase item y contador de objetos carrito)
 const items = document.querySelectorAll(".item")
-
-// Obtener el elemento del contador general
 const elementCounter = document.getElementById("element-counter")
-updateElementCounter()
+const billElement = document.getElementById("factura-div")
+const payBtn = document.getElementById("pay-btn")
+//Se espera a cargar el DOM para cargar los archivos
 document.addEventListener("DOMContentLoaded", () => {
+    updateElementCounter()
     loadItems()
-    loadCart()
-    if (elementCounter.innerText != "0") {
-        elementCounter.style.opacity = "1"
+    // Comprobaciones inicial
+    try {
+        if (elementCounter.innerText != "0") {
+            elementCounter.style.opacity = "1"
+        }
+        if (localStorage.getItem("cart") == null) {
+            elementCounter.style.opacity = "0"
+        }
+    } catch {
+        console.log("Cart element not found")
     }
-    //updateElementCounter()
+    try {
+        //createBill()
+        if (localStorage.getItem("billData") == "<hr><li>Total: 0.00$</li>") {
+            billElement.innerHTML = ""
+        }
+    } catch (e) {
+        console.log("Cannot create bill")
+    }
 })
-// Recorrer cada elemento y agregar el evento de clic a los botones de incremento y decremento
+//Se recorre cada item
 items.forEach((item) => {
     const incrementButton = item.querySelector(".btn-increment")
     const decrementButton = item.querySelector(".btn-decrement")
     const counterSpan = item.querySelector(".item-buttons span")
-
-    // Obtener el valor guardado en el localStorage para este item
-    const savedValue = localStorage.getItem(item.id)
-
-    // Verificar si hay un valor guardado en el localStorage
-    if (savedValue !== null) {
-        // Restaurar el valor del contador desde el localStorage
-        counterSpan.innerText = savedValue
-    }
-
+    //Evento de incremento
     incrementButton.addEventListener("click", () => {
-        // Obtener el valor actual del contador y convertirlo a número
         let counterValue = parseInt(counterSpan.innerText)
-
-        // Aumentar el valor del contador
         counterValue++
-
-        // Actualizar el valor del contador en el span
         counterSpan.innerText = counterValue.toString()
-
-        // Guardar el valor en el localStorage para este item
-        localStorage.setItem(item.id, counterValue.toString())
-
-        // Actualizar el contador general
+        //Actualiza contador carrito
         updateElementCounter()
+        //Refresca la factura
+        createBill()
+        // Guardar cantidad en cada item el localStorage
+        saveItems()
     })
-
+    // Evento de decremento
     decrementButton.addEventListener("click", () => {
-        // Obtener el valor actual del contador y convertirlo a número
         let counterValue = parseInt(counterSpan.innerText)
 
-        // Verificar que el valor del contador sea mayor que cero antes de decrementar
         if (counterValue > 0) {
-            // Decrementar el valor del contador
             counterValue--
-
-            // Actualizar el valor del contador en el span
             counterSpan.innerText = counterValue.toString()
-
-            // Guardar el valor en el localStorage para este item
-            localStorage.setItem(item.id, counterValue.toString())
-
-            // Actualizar el contador general
-            updateElementCounter()
         }
+        //Actualiza contador carrito
+        updateElementCounter()
+        //Refresca la factura
+        createBill()
+        //En caso de que la factura quede vacía
+        if (billElement.innerHTML == "<ul><hr><li>Total: 0.00$</li></ul>") {
+            billElement.innerHTML = ""
+        }
+        // Guardar cantidad en cada item el localStorage
+        saveItems()
     })
 })
+try {
+    payBtn.addEventListener("click", () => {
+        let total = 0
+        items.forEach((item) => {
+            const counterSpan = parseInt(item.querySelector(".item-buttons span").innerText)
+            total += counterSpan
+        })
+        if (total > 0) {
+            window.location.href = "payment.html"
+        } else {
+            alert("Debe seleccionar al menos un item")
+        }
+    })
+} catch (e) {
+    console.log("pay button not found")
+}
+//FUNCIONES
 
 // Función para actualizar el contador general
 function updateElementCounter() {
     let total = 0
-
-    // Obtener el valor de cada contador individual y sumarlo al total
+    // Obtener la cantidad total de items
     items.forEach((item) => {
-        const counterSpan = item.querySelector(".item-buttons span")
-        const counterValue = parseInt(counterSpan.innerText)
-        total += counterValue
-
-        // Guardar el valor en el localStorage para este item
-        localStorage.setItem(item.id, counterValue.toString())
+        const counterSpan = parseInt(item.querySelector(".item-buttons span").innerText)
+        total += counterSpan
     })
 
-    // Actualizar el valor del contador general
-    if (total > 9) {
-        elementCounter.innerText = "+9"
-    } else {
-        elementCounter.innerText = total.toString()
-    }
-
-    // Actualizar la opacidad del span
-    if (total === 0) {
-        elementCounter.style.opacity = "0"
-    } else {
-        elementCounter.style.opacity = "1"
-    }
-}
-try {
-    facturaElement = document.getElementById("factura-div")
-    const cantidadElementos = {}
-    const ordenIngreso = []
-    facturaElement.innerHTML = localStorage.getItem("facturaText")
-    items.forEach((item) => {
-        const btnIncrement = item.querySelector(".btn-increment")
-        const btnDecrement = item.querySelector(".btn-decrement")
-
-        const itemName = item.querySelector(".item-content span").innerText
-        const itemPrecio = parseFloat(item.querySelector(".item-price").innerText.replace("$", ""))
-
-        // Inicializar la cantidad del elemento en 0
-        cantidadElementos[itemName] = 0
-        // Agregar el nombre del elemento al array de orden de ingreso
-        ordenIngreso.push(itemName)
-
-        btnIncrement.addEventListener("click", () => {
-            cantidadElementos[itemName]++
-            const facturaText = generarFacturaText()
-
-            facturaElement.innerHTML = facturaText
-
-            // Guardar el contenido de factura-div en el localStorage
-            localStorage.setItem("facturaText", facturaText)
-
-            console.log(facturaText)
-        })
-
-        btnDecrement.addEventListener("click", () => {
-            cantidadElementos[itemName] = Math.max(0, cantidadElementos[itemName] - 1)
-            const facturaText = generarFacturaText()
-            facturaElement.innerHTML = facturaText
-
-            // Guardar el contenido de factura-div en el localStorage
-            localStorage.setItem("facturaText", facturaText)
-
-            console.log(facturaText)
-        })
-    })
-    // FIXME Arreglar problemas con las facturas al refrescar
-    function generarFacturaText() {
-        let facturaText = ""
-        let total = 0
-        // Recorrer el array de orden de ingreso
-        for (const itemName of ordenIngreso) {
-            const count = cantidadElementos[itemName]
-            if (count > 0) {
-                let itemPrecio
-                for (const item of items) {
-                    if (item.querySelector(".item-content span").innerText === itemName) {
-                        itemPrecio = parseFloat(
-                            item.querySelector(".item-price").innerText.replace("$", "")
-                        ).toFixed(2)
-
-                        break
-                    }
-                }
-
-                const subtotal = (count * itemPrecio).toFixed(2)
-                total += parseFloat(subtotal)
-                facturaText += `<li>${itemName}: ${count} x ${itemPrecio + "$"} = ${subtotal}</li>`
-            }
+    // Valores posibles
+    try {
+        if (total > 9) {
+            elementCounter.innerText = "+9"
+        } else {
+            elementCounter.innerText = total.toString()
         }
-        facturaText += `<li>Total: ${total}</li>`
-        saveItems()
-        return facturaText ? `<ul>${facturaText}</ul>` : ""
-    }
-} catch (error) {
-    console.log(error)
-}
 
+        // Aparecer/Ocultar el contador
+        if (total === 0) {
+            elementCounter.style.opacity = "0"
+        } else {
+            elementCounter.style.opacity = "1"
+        }
+    } catch (e) {
+        console.log("Element counter not found")
+    }
+}
+// Función para guardar el contador del carrito y los items en el localStorage
 function saveItems() {
-    const valores = []
-    // Obtener el valor de cada contador individual y sumarlo al total
+    //Array con cada contador de los items
+    const values = []
     items.forEach((item) => {
         const counterSpan = item.querySelector(".item-buttons span")
-        valores.push(counterSpan.innerText)
+        values.push(counterSpan.innerText)
     })
     localStorage.setItem("cart", elementCounter.innerText)
     localStorage.removeItem("items")
-    localStorage.setItem("items", JSON.stringify(valores))
-    console.log(valores)
+    localStorage.setItem("items", JSON.stringify(values))
+    console.log("values saved: " + values)
+    localStorage.setItem("billData", billElement.innerHTML)
 }
-
+// Función para cargar los items del localStorage en el DOM
 function loadItems() {
-    const storedItems = JSON.parse(localStorage.getItem("items")) || []
+    //Carrito
+    try {
+        elementCounter.innerText = localStorage.getItem("cart")
+        console.log("cart loaded: " + elementCounter.innerText)
+    } catch (e) {
+        console.log("Cart element not found")
+    }
 
+    //Items individuales
+    const storedItems = JSON.parse(localStorage.getItem("items")) || []
+    console.log("loaded items: " + storedItems)
     items.forEach((item, index) => {
         const counterSpan = item.querySelector(".item-buttons span")
         counterSpan.innerText = storedItems[index] || "0"
     })
+    //Factura
+    try {
+        billElement.innerHTML = localStorage.getItem("billData")
+    } catch (e) {
+        console.log("Bill element not found")
+    }
 }
-function loadCart() {
-    elementCounter.innerText = localStorage.getItem("cart")
-    console.log(elementCounter.innerText)
+// Función para crear la factura
+function createBill() {
+    billElement.innerHTML = ""
+    let total = 0
+    //Crea la lista desordenada
+    let bill = document.createElement("ul")
+    //Recorre cada item
+    items.forEach((item) => {
+        const itemName = item.querySelector(".item-content span").innerText
+        const itemPrice = parseFloat(item.querySelector(".item-price").innerText.replace("$", "")).toFixed(2)
+        const itemAmount = parseInt(item.querySelector(".item-buttons span").innerText)
+
+        //Si la cantidad del item es mayor a cero
+        if (itemAmount > 0) {
+            total += itemAmount * itemPrice
+            //Agrega el item a la lista
+            bill.innerHTML += `<li>${itemName}: ${itemAmount} x ${itemPrice + "$"} = ${(itemAmount * itemPrice).toFixed(2) + "$"}</li>`
+        }
+    })
+    //Agrega el total
+    bill.innerHTML += `<hr>`
+    bill.innerHTML += `<li>Total: ${total.toFixed(2) + "$"}</li>`
+
+    //Guarda la lista en el localStorage
+    localStorage.setItem("billData", bill.innerHTML)
+    billElement.appendChild(bill)
 }
